ac4d3e194c3161141ef64f1a10f7800667df68fb
maitrey684 <patel_maitrey684@yahoo.co.in>
2021-10-12 11:25:07 +0530

    Updated initial co-ordinates

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 function initAutocomplete() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -33.8688, lng: 151.2195 },
+    center: { lat: 23.0674, lng: 72.5357 },
     zoom: 17,
     mapTypeId: "roadmap",
   });
